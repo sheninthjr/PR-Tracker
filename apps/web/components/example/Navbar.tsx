5d5fcc85ad0bde4b800270edaa416f0c912bae22
sheninthjr <sheninthjr23@gmail.com>
2024-08-07 22:57:59 +0530

    NavBar added

---
+++ apps/web/components/example/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+export function Navbar() {
+  return (
+    <nav className="fixed bg-gray-950 p-4">
+      <div className="flex justify-start items-center max-w-full mx-0">
+        <div className="text-white text-2xl font-bold">
+          <Link href="/">PR Tracker</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
