f7ba4a804e1d9a04c47a3c49847108d8d33537de
sheninthjr <sheninthjr23@gmail.com>
2024-09-16 20:27:29 +0530

    Created a fresh Next app

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
-import { Navbar } from '@/components/example/Navbar';
-import { Toaster } from '@/components/ui/toaster';
-import RecoilRootWrapper from '@/lib/recoilRootWrapper';
-const inter = Inter({ subsets: ['latin'] });
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,12 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <RecoilRootWrapper>
-          <Navbar />
-          {children}
-          <Toaster />
-        </RecoilRootWrapper>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
--- apps/web/app/repo/[repoId]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function RepoId({ params }: { params: { repoId: string } }) {
-  return (
-    <>
-      <div className="text-white flex justify-center h-screen text-5xl">
-        My Post: {params.repoId}
-      </div>
-    </>
-  );
-}
